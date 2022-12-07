16a516ef73ea857cd7868b7f17387d39f461abf3
anderson_ocana <aocanatorres@buenosaires.gob.ar>
2022-12-07 10:09:55 -0300

    django_crud

--- crud_tareas/crud_tareas/settings.py
+++ crud_tareas/crud_tareas/settings.py
@@ -145,9 +145,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    # os.path.join(BASE_DIR,'static'),
 ]
  
 
