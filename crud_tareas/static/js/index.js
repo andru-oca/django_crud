e5ea83820e75893bb3ae3f165335b65eb53d5041
anderson_ocana <aocanatorres@buenosaires.gob.ar>
2022-12-07 10:23:17 -0300

    django_crud

--- crud_tareas/static/js/index.js
+++ crud_tareas/static/js/index.js
@@ -1,10 +1,3 @@
-const URL = 'https://catfact.ninja/facts'
-
-fetch(URL)
-.then(data => data.json())
-.then(data => injecter(data.data))
-.catch(e => console.log(e))
-
 
 function injecter(data){
 
@@ -16,7 +9,7 @@ function injecter(data){
     if (to_delete != null){
 
         appender.removeChild(to_delete)
-    }
+    }    
   
     let whole = ''
     
@@ -37,32 +30,73 @@ function injecter(data){
             
             <div class="item">
                ${itemList++} :  ${i.fact}
-            </div>
+            </div>   
                 
             ` ; 
             
             whole += data_appender ; 
 
-        }
+        }    
 
-    }
+    }    
     else{
             
         whole = '<h2> No hay data disponible </h2>'
-    }
+    }    
     
     wholeNode.innerHTML = whole;
     
     appender.appendChild(wholeNode);
     
+}    
+
+function injector(data){
+
+    let appender = document.getElementById('details')
+
+    let to_delete = document.querySelector('#details> div');
+
+    if (to_delete != null){
+
+        appender.removeChild(to_delete)
+    }
+ 
+
+    let wholeNode = document.createElement('div');
+    
+    let data_appender=`
+    
+    <div class="item">
+    <h1>${data.nombre}</h1> 
+    
+        <ul>
+        <li>Descripcion: ${data.descripcion}</li>
+        <li>Horario de inicio de la tarea:  ${data.horario_inicio}</li>
+        <li>Dia a realizarlo: ${data.dia_realizar}</li>
+        </ul>
+    
+    </div>
+    
+    ` ; 
+    
+    wholeNode.innerHTML = data_appender;
+    appender.appendChild(wholeNode);   
 }
 
-http://ander1987.pythonanywhere.com/api/todo/1/
 
 
 
-let base_host = 'http://ander1987.pythonanywhere.com/api'
-let todo_list_url = '/todo/'
+const URL = 'https://catfact.ninja/facts'
+
+fetch(URL)
+.then(data => data.json())
+.then(data => injecter(data.data))
+.catch(e => console.log(e))
+
+
+let base_host = 'http://ander1987.pythonanywhere.com/api/'
+let todo_list_url = 'todo/'
+
 let path
 
 
@@ -83,52 +117,13 @@ document.addEventListener('click',
                 .then(e => e.json())
                 .then(e=> injector(e))
 
-            }
+            }    
 
             return
 
-        }
+        }    
         
-    }
-)
-
-
-function injector(data){
-
-    let appender = document.getElementById('details')
-
-    let to_delete = document.querySelector('#details> div');
-
-    if (to_delete != null){
-
-        appender.removeChild(to_delete)
-    }
-  
-
-    let wholeNode = document.createElement('div');
-    
-    
-    
-    let data_appender=`
-    
-    <div class="item">
-    <h1>${data.nombre}</h1> 
-    
-        <ul>
-        <li>Descripcion: ${data.descripcion}</li>
-        <li>Horario de inicio de la tarea:  ${data.horario_inicio}</li>
-        <li>Dia a realizarlo: ${data.dia_realizar}</li>
-        </ul>
-    
-    </div>
-    
-    ` ; 
-    
-    wholeNode.innerHTML = data_appender;
-
-    
-    appender.appendChild(wholeNode);
-    
-}
+    }    
+)    
 
 
